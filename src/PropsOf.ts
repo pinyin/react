0b36de45b1443b1fdee28df42d80cb55aa7abc76
pinyin <bob432091@gmail.com>
2018-05-04 23:06:17 +0800

    0.0.4 delete unnecessary dependencies

--- src/PropsOf.ts
+++ src/PropsOf.ts
@@ -1,13 +1,15 @@
-import {Component, ComponentClass, ComponentType, DetailedHTMLFactory, ReactHTML, StatelessComponent} from 'react'
+import {Component, ComponentClass, DetailedHTMLFactory, ReactHTML, StatelessComponent} from 'react'
 
 export type PropsOf<T extends ComponentClass | Component | StatelessComponent | keyof ReactHTML> =
-    T extends ComponentType<infer R> ?
+    T extends ComponentClass<infer R> ?
         R :
-        T extends Component<infer R> ?
+        T extends StatelessComponent<infer R> ?
             R :
-            T extends keyof ReactHTML ?
-                ReactHTML[T] extends DetailedHTMLFactory<infer R, any> ?
-                    R :
-                    never :
-                never
+            T extends Component<infer R> ?
+                R :
+                T extends keyof ReactHTML ?
+                    ReactHTML[T] extends DetailedHTMLFactory<infer R, any> ?
+                        R :
+                        never :
+                    never
 
