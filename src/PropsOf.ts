94865c9ceaa4e3c57f4a332deac777ab49852980
pinyin <bob432091@gmail.com>
2018-06-06 06:51:00 +0800

    don't accept HTMLElement in PropsOf

--- src/PropsOf.ts
+++ src/PropsOf.ts
@@ -1,6 +1,6 @@
 import {Component, ComponentClass, ComponentType, DetailedHTMLFactory, DOMFactory, ReactHTML, ReactSVG, StatelessComponent} from 'react'
 
-export type PropsOf<T extends ComponentType<any> | ComponentClass<any> | Component<any, any> | StatelessComponent<any> | keyof ReactHTML | keyof ReactSVG | HTMLElement> =
+export type PropsOf<T extends ComponentType<any> | ComponentClass<any> | Component<any, any> | StatelessComponent<any> | keyof ReactHTML | keyof ReactSVG> =
     T extends ComponentType<infer R> ?
         R :
         T extends ComponentClass<infer R> ?
